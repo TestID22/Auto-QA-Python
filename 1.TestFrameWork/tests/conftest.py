7f485d7860f99741ab15385483de753be0bfe9fe
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-07 12:59:08 +0300

    Added Base Page -fixed typo in name

--- 1.TestFrameWork/tests/conftest.py
+++ 1.TestFrameWork/tests/conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="function", autouse=True)
 def driver():
     """driver configuration"""
+    options = Options()
+    #options.add_argument("--headless")
+    options.add_argument("no-sandbox")
     driver_service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=driver_service)
     driver.maximize_window()
