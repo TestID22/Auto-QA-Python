dbfbf5f71c5f17f5cee2479dd1b59ce550c6d91a
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-04-09 09:08:33 +0300

    first working test

--- 1.TestFrameWork/tests/BaseTest.py
+++ 1.TestFrameWork/tests/BaseTest.py
@@ -1,20 +1,20 @@
 import pytest
 
-from pages import LoginPage
-from pages import DashboardPage
-from pages import PersonalPage
-
+from pages.LoginPage import LoginPage
+from pages.DashboardPage import DashboardPage
+from pages.PersonalPage import PesrsonalPage
+from config.Data import Data
 
 class BaseTest:
 
     login_page: LoginPage
     dashboard_page: DashboardPage
-    personal_Page: PersonalPage
-
+    personal_Page: PesrsonalPage
+    data: Data
     @pytest.fixture(autouse=True)
     def setup(self, request, driver):
         request.cls.driver = driver
         request.cls.login_page = LoginPage(driver)
         request.cls.dashboard = DashboardPage(driver)
-        request.cls.login_page = PersonalPage(driver)
-
+        request.cls.personal_page = PesrsonalPage(driver)
+        request.cls.data = Data()
--- 1.TestFrameWork/tests/InfoPageTest.py
+++ 1.TestFrameWork/tests/InfoPageTest.py
@@ -1,7 +1,8 @@
 from BaseTest import BaseTest
-
-class InfoPageTest(BaseTest):
+class Test_InfoPage(BaseTest):
 
 
     def test_logining(self):
-        self.login_page.eneter_login()
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
